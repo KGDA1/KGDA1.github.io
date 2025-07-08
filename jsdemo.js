c2d5e6a54a4af23d154fa25e1d588a44c011715a
KGDA1 <kylangaines2007@gmail.com>
2025-07-08 16:43:16 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -2,3 +2,11 @@ console.log("This output is from the external JS file");
 console.log("name: Kylan");
 console.log("school: CPCC");
 console.log("Grade: Graduate");
+
+var name = "Kylan";
+let school = "CPCC"
+var grade = "graduate";
+
+document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
+document.getElementById("grade").innerHTML = "Grade: <b>" + Grade + "</b>";
+document.getElementById("school").innerHTML = "school: <b>" + school + "</b>";
