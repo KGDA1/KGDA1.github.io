2578a32dc724b1b780ef50b7e4adb4e857227287
KGDA1 <kylangaines2007@gmail.com>
2025-07-17 16:47:03 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -10,3 +10,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let circleMarker = L.circle(coordinates,{
+ color: "Red", 
+   fillColor: "Gold",
+  fill0pacity: 0.25,
+  radius; 50
+}).bindPopup(`<iframe width="280" height="160" src="https://www.youtube.com/embed/5tRNJ12nSIY?si=TWpneRoNRGZLfqWO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`).addTo(map);
